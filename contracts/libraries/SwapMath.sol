e2183f5338646f08748f140e04e7163cb88356b7
Mike <manhlx.300695@gmail.com>
2023-05-08 14:16:42 +0700

    Remove assertion

--- contracts/libraries/SwapMath.sol
+++ contracts/libraries/SwapMath.sol
@@ -77,9 +77,6 @@ library SwapMath {
       );
       nextSqrtP = calcFinalPrice(absDelta, liquidity, deltaL, currentSqrtP, isExactInput, isToken0)
       .toUint160();
-      // safe check to ensure nextSqrtP shouldn't pass targetSqrtP
-      if (isToken0 != isExactInput) assert(nextSqrtP <= targetSqrtP);
-      else assert(nextSqrtP >= targetSqrtP);
     } else {
       deltaL = calcIncrementalLiquidity(
         absDelta,
